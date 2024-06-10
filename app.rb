a8d418135c50df2875d3889d334664ed9cce6336
hossam <hossam>
2024-06-10 05:00:23 +0300

    first commit

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
+require "byebug"
 
 set :public_folder, 'public'
 set :port, 4000
 
 get '/' do
-  cache_control :public, max_age: 3600
+  #cache_control :public, max_age: 3600
   send_file File.join(settings.public_folder, 'index.html')
+end
+
+
+post '/send_email' do
+  byebug
+  "#{params}"
 end
